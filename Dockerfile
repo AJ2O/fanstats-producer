604087e197fd098e8fb7412c415af1421788ec8d
Juwon44 <juwonobat@gmail.com>
2021-03-19 23:48:28 -0400

    restructured template files

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # provide environment variables securely
 ENV DATA_FILE=nba.yaml
+ENV PLATFORMS_FILE=platforms.yaml
 ENV STORAGE_BUCKET=bucket_in_S3
 ENV TWITTER_BEARER_TOKEN=twitter_bearer_token
-ENV CONFIG_SETUP=True
+ENV ENVIRONMENT_SETUP=True
 
 COPY ./src .
 CMD [ "python", "./main.py" ]
--- src/main.py
+++ src/main.py
@@ -9,18 +9,21 @@ parser.add_argument(
     metavar='N', 
     type=int, 
     default=100,
-    help='The number of Twitter results to retrieve for every page request')
+    help='The number of Twitter results to retrieve per page request')
 parser.add_argument(
     '-m',
     '--twitter_max_results', 
     metavar='N',
     type=int, 
-    default=100,
-    help='The maximum number of Twitter results to retrieve per item')
+    default=3000,
+    help='The maximum number of Twitter results to retrieve')
 args = parser.parse_args()
 
+# constants / environment variables
 RESULTS_PER_PAGE = args.twitter_results_per_page
-MAX_RESULTS_PER_ITEM = args.twitter_max_results
+MAX_RESULTS = args.twitter_max_results
+STORAGE_BUCKET = ''
+TWITTER_BEARER_TOKEN = ''
 
 # import libraries
 import os
@@ -29,41 +32,38 @@ import json
 import ast
 import yaml
 import boto3
-from datetime import datetime
+import datetime
 from time import gmtime, strftime
 
 # AWS clients
 s3_client = boto3.client('s3')
 
-# environment variables
-storage_bucket = ''
-twitter_bearer_token = ''
-
-# supported social media platforms
-supported_platforms = [
-    'Twitter'
-]
-
-# helpers
-def load_env_config():
-    if not os.environ.get('CONFIG_SETUP'):
+# file helpers
+def load_environment_variables():
+    if not os.environ.get('ENVIRONMENT_SETUP'):
+        # TODO: download config file
         with open('config.yaml') as file:
             config = yaml.safe_load(file)
             os.environ.update(config)
         
-def load_league_data(league_file):
-    with open(league_file) as file:
-        return yaml.safe_load(file)
+def load_data_file(data_file):
+    with open(data_file) as file:
+        documents = list(yaml.safe_load_all(file))
+        version = documents[0]
+        data = documents[1]
+        return version, data
 
-def clean_text(text):
-    '''
-    Formats text nicely for pandas by interpreting/replacing problematic characters.
-    '''
-    return text.replace("\n", "\\n")
+def load_platforms(platforms_file):
+    with open(platforms_file) as file:
+        documents = list(yaml.safe_load_all(file))
+        version = documents[0]
+        data = documents[1]
+        return version, data
 
 # Twitter helpers
 def create_twitter_url(
     entity,
+    start_time='2021-01-00T00:00:00.00Z',
     max_results=10, 
     next_token=None,
     query_args="-is:retweet lang:en -%23nbatopshot",
@@ -78,12 +78,14 @@ def create_twitter_url(
     query_args_fmt = "query=entity:\"{}\"{}".format(entity, query_args)
     max_results_fmt = "max_results={}".format(max_results)
     tweet_fields_fmt = "tweet.fields={}".format(tweet_fields)
+    start_time_fmt = "start_time={}".format(start_time)
     next_token_fmt = "&next_token={}".format(next_token) if next_token else ""
 
-    url = "https://api.twitter.com/2/tweets/search/recent?{}&{}&{}{}".format(
+    url = "https://api.twitter.com/2/tweets/search/recent?{}&{}&{}&{}{}".format(
         query_args_fmt,
         max_results_fmt,
         tweet_fields_fmt,
+        start_time_fmt,
         next_token_fmt
     )
     return url
@@ -102,14 +104,14 @@ def get_team_data_from_twitter(team):
     results_counted = 0
     next_token = None
 
-    while results_counted < MAX_RESULTS_PER_ITEM:
+    while results_counted < MAX_RESULTS:
         # load tweets
         url = create_twitter_url(
             entity=team, 
             max_results=RESULTS_PER_PAGE, 
             next_token=next_token
         )
-        res_twitter = twitter_auth_and_connect(twitter_bearer_token, url)
+        res_twitter = twitter_auth_and_connect(TWITTER_BEARER_TOKEN, url)
 
         # add data
         if 'data' in res_twitter:
@@ -157,26 +159,149 @@ def write_all_team_data(
 
     return data
 
+
+# Twitter, data = v1.0
+def get_tweets(
+    start_date,
+    topic='NBA'
+    ):
+    # collect tweets
+    tweets = []
+    results_counted = 0
+    next_token = None
+
+    while results_counted < MAX_RESULTS:
+        # load tweets
+        isotime = start_date.isoformat() + "Z"
+        url = create_twitter_url(
+            entity=topic,
+            start_time=isotime,
+            max_results=RESULTS_PER_PAGE, 
+            next_token=next_token
+        )
+        res_twitter = twitter_auth_and_connect(TWITTER_BEARER_TOKEN, url)
+
+        # add data
+        if 'data' in res_twitter:
+            tweets.extend(res_twitter['data'])
+
+        # end loop early if there is no next_token
+        results_counted += res_twitter['meta']['result_count']
+        if 'next_token' in res_twitter['meta']:
+            next_token = res_twitter['meta']['next_token']
+        else:
+            break
+    return tweets
+
+def write_twitter_data(
+    data,
+    start_date,
+    datafile_version=1.0
+    ):
+    if datafile_version == 1.0:
+        topic = data['topic']
+        topic_type = data['type']
+        aliases = data['aliases']
+
+        # Twitter API v2 is in JSON format
+        output_time = datetime.datetime.utcnow()
+        output_time.microsecond = 0
+        output_file = str(output_time) + ".json"
+
+        tweets = get_tweets(start_date, topic=topic)
+        # write json output
+        with open(output_file, 'a') as outfile:
+            for tweet in tweets:
+                outfile.write(json.dumps(tweet) + '\n')
+
+        # upload file to S3
+        s3_key_partition = topic
+        if topic_type == 'Team':
+            s3_key_partition = data['League']
+
+        s3_key = "Twitter/" + s3_key_partition + "/" + output_file
+        s3_client.upload_file(
+            Filename=output_file,
+            Bucket=STORAGE_BUCKET,
+            Key=s3_key
+        )
+        print("Uploaded data to s3://" + s3_key)
+    else:
+        raise(
+            "Datafile version " 
+            + datafile_version 
+            + " is unsupported for Twitter"
+        )
+
+# platform = v1.0
+def collect_and_write_all_platform_data_v1_0(
+    data,
+    platforms_data,
+    start_date,
+    datafile_version=1.0
+    ):
+    platforms = platforms_data['platforms']
+    print(str(len(platforms)) + " social media platforms: " + str(platforms))
+    for p in platforms:
+        print("Collecting " + p + " data...")
+        if p == 'Twitter':
+            write_twitter_data(data, start_date, datafile_version)
+        else:
+            raise("Social media platform " + p + " is unsupported")
+
+def collect_and_write_data(
+    data,
+    platforms_data,
+    start_date,
+    datafile_version=1.0,
+    platformfile_version=1.0,
+    ):
+    if platformfile_version == 1.0:
+        collect_and_write_all_platform_data_v1_0(
+            data=data,
+            platforms_data=platforms_data,
+            start_date=start_date,
+            datafile_version=datafile_version
+        )
+    else:
+        raise("Platformfile version " + platformfile_version + " is unsupported")
+
 # main
 def main():
-    # set global variables
-    global storage_bucket
-    global twitter_bearer_token
+    global STORAGE_BUCKET
+    global TWITTER_BEARER_TOKEN
 
     # set environment variables
-    load_env_config()
+    load_environment_variables()
     source_data_file = os.environ.get('DATA_FILE')
-    storage_bucket = os.environ.get('STORAGE_BUCKET')
-    twitter_bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
+    platforms_file = os.environ.get('PLATFORMS_FILE')
+    STORAGE_BUCKET = os.environ.get('STORAGE_BUCKET')
+    TWITTER_BEARER_TOKEN = os.environ.get('TWITTER_BEARER_TOKEN')
 
     # load data file
-    data = load_league_data(source_data_file)
-    league = data['league']
-    print("Loading data for the league: " + league)
-    teams = data['teams']
+    datafile_version, data = load_data_file(source_data_file)
+    print("Datafile Version: " + str(datafile_version['version']))
 
-    # load data for each supported social media platform
-    for platform in supported_platforms:
+    # load social media platforms file
+    platformfile_version, platforms_data = load_platforms(platforms_file)
+    print("Platformfile Version: " + str(platformfile_version['version']))
+
+    # start date (past 24 hours)
+    start_date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    print("Retrieving data from " + str(start_date))
+
+    # collect and write data
+    collect_and_write_data(
+        data=data,
+        platforms_data=platforms_data,
+        start_date=start_date,
+        datafile_version=datafile_version['version'],
+        platformfile_version=platformfile_version['version']
+    )
+    print("Finished collecting all data!")
+
+    ''' load data for each given social media platform
+    for platform in social_media_platforms:
         print("Started collecting " + platform + " data...")
 
         ### Team Data Start
@@ -199,17 +324,18 @@ def main():
             + strftime("%Y/%m/%d/%H-%M-%S", gmtime()) + ".json"
         s3_client.upload_file(
             Filename=output_file,
-            Bucket=storage_bucket,
+            Bucket=STORAGE_BUCKET,
             Key=s3_key
         )
         print("Uploaded " 
             + platform 
-            + " team data results to s3://" + storage_bucket + "/" + s3_key)
+            + " team data results to s3://" + STORAGE_BUCKET + "/" + s3_key)
         ### Team Data End
 
         print("Finished collecting " + platform + " data!")
-
+    
     print("Finished " + league + " data collection!")
+    '''
 
 if __name__ == "__main__":
     main()
